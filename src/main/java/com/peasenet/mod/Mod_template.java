3041b4bc709fa2890c2e165d5149aceda2cba225
Gavin <gavinpease@gmail.com>
2023-07-14 13:00:22 -0600

    Add base template mod

--- src/main/java/com/peasenet/mod/Mod_template.java
+++ src/main/java/com/peasenet/mod/Mod_template.java
@@ -1,16 +1,20 @@
 package com.peasenet.mod;
 
+import com.peasenet.main.GavinsMod;
+import com.peasenet.mod.misc.ModExample;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mod_template implements ModInitializer {
     public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
+
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
         LOGGER.info("Hello Fabric world!");
+        GavinsMod.addMod(new ModExample());
     }
 }
