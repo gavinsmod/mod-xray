d049092db63cc981ece39d8080ca9ca2c4a1c082
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:20 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/XrayMod.java
+++ src/main/java/com/peasenet/mod/XrayMod.java
@@ -9,16 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class XrayMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing GEM Xray!");
         Settings.addConfig(new XrayConfig());
         GavinsMod.addMod(new ModXray());
-        LOGGER.info("Initialized GEM Xray!");
     }
 }
